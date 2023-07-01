83c3da6cf9f025a61a2209d1ffaa32ab01ee5043
krishnan74 <98215383+krishnan74@users.noreply.github.com>
2023-07-01 19:06:07 +0530

    Menu

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject loadingPanel; 
+    // Start is called before the first frame update
+    public void PlayGame(){
+
+        slider.value = 0f; 
+        loadingPanel.SetActive(true);
+        StartCoroutine(LoadAsynchronously("dk"));
+    }  
+
+    IEnumerator LoadAsynchronously(string sceneName){
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        
+        while(!operation.isDone){  
+            float progress = Mathf.Clamp01(operation.progress / .9f); 
+            slider.value = progress;
+            yield return null; 
+        }
+    }
+}
