1b614c201164383cc4b68ebbf653523d5b613edf
Speckles <speckleszzzz@gmail.com>
2023-07-12 16:23:17 +0530

    Dialogue

---
+++ Assets/DialogueeManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Image actorImage;
+    public Text actorName;
+    public Text messageText;
+    public RectTransform backgroundBox;
+
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+    public static bool isActive = false;
+
+    public void OpenDialogue(Message[] messages,Actor[] actors)
+    {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+        isActive = true;
+        Debug.Log("started" + messages.Length);
+        DisplayMessages();
+
+    }
+
+    void DisplayMessages()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorName.text = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite; 
+    }
+
+    public void NextMessage()
+    {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length)
+        {
+            DisplayMessages();
+        }
+        else
+        {
+            Debug.Log("convo done");
+            isActive = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive==true)
+        {
+            NextMessage();
+        }
+    }
+}
