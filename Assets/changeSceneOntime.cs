ce1071b4b0c89c14d00eac9f9b431141a1f95319
krishnan74 <98215383+krishnan74@users.noreply.github.com>
2023-07-12 16:01:05 +0530

    cutsceneLoad

--- Assets/changeSceneOntime.cs
+++ Assets/changeSceneOntime.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class changeSceneOntime : MonoBehaviour
 {
+
+    public Slider slider;
+    public GameObject loadingPanel; 
     public float changeTime;
     public string sceneName;
 
     void Start()
     {
-        StartCoroutine(changeSceneOnT());
+        slider.value = 0f; 
+        StartCoroutine(LoadAsynchronously(sceneName));
     }
 
-    private IEnumerator changeSceneOnT()
-    {
+
+    IEnumerator LoadAsynchronously(string sceneName){
         yield return new WaitForSeconds(changeTime);
-        SceneManager.LoadScene(sceneName);
+        
+        loadingPanel.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        
+        while(!operation.isDone){  
+            float progress = Mathf.Clamp01(operation.progress / .9f); 
+            slider.value = progress;
+            yield return null; 
+        }
     }
 
 }
