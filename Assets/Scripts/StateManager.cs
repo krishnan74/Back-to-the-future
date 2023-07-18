c22c4a3aee814ce0c6115564db3f203bfa71265b
krishnan74 <98215383+krishnan74@users.noreply.github.com>
2023-07-18 12:53:38 +0530

    Game Complete Version1

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -7,7 +7,7 @@ namespace FlowControllerlast
 {
     public class StateManager : MonoBehaviour
     {
-        public static BigInteger plutoCount = 10000;
+        public static BigInteger plutoCount = 0;
         public static UnityEngine.Vector3 LastPosition = new UnityEngine.Vector3(-5.18f, -3.2f, 0f);
         public static Dictionary<string, string> emulatorAccountDictionary = new Dictionary<string, string>();
         public static Dictionary<string, string> testAccountDictionary = new Dictionary<string, string>();
